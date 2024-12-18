dcd4c92fde5b59de1ac6ee7c49bd56342633347f
Mohmmed Abuhasna <38439020+mohammedhasna2015@users.noreply.github.com>
2024-12-19 00:04:30 +0200

    add testing for payment

--- test/features/withdraw/data/data_soruces/withdraw_datasource_test.dart
+++ test/features/withdraw/data/data_soruces/withdraw_datasource_test.dart
@@ -10,6 +10,7 @@ import 'package:thrivve_assignment/core/base/exceptions.dart';
 // Import the class to be tested and its dependencies
 import 'package:thrivve_assignment/features/withdraw/data/data_soruces/i_withdraw_datasource.dart';
 import 'package:thrivve_assignment/features/withdraw/data/data_soruces/withdraw_datasource.dart';
+import 'package:thrivve_assignment/features/withdraw/data/models/payment_model.dart';
 import 'package:thrivve_assignment/features/withdraw/data/models/withdraw_confirm_model.dart';
 import 'package:thrivve_assignment/features/withdraw/data/services/withdraw_service.dart';
 
@@ -94,6 +95,67 @@ void main() {
         () => dataSource.confirmWithDraw(),
         throwsA(isA<ServerException>()),
       );
+    });
+  });
+  group('listPayment', () {
+    test('listPayment returns List<PaymentModel> successfully', () async {
+      final resultJson = [
+        {
+          "status": "In-Review",
+          "beneficiary_iban": "GB29NWBK60161331926819",
+          "bank_image":
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn_bhPgvIrnWNmBgZz5YYe5rjflSYjf9WhEDa_Ia0&s",
+          "bank_id": 4,
+          "bank_name": "Al Rajhi"
+        },
+        {
+          "status": null,
+          "beneficiary_iban": "AR29NWBK60161331926820",
+          "bank_image":
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn_bhPgvIrnWNmBgZz5YYe5rjflSYjf9WhEDa_Ia0&s",
+          "bank_id": 4,
+          "bank_name": "Al Arabi"
+        }
+      ];
+      final mockHttpResponse = http.Response(
+        jsonEncode(resultJson),
+        200,
+      );
+
+      final mockResponse = Response(mockHttpResponse, resultJson);
+
+      // Setup mock service to return the mock response
+      when(mockWithdrawService.listPayment())
+          .thenAnswer((_) async => mockResponse);
+
+      // Call the method
+      final result = await dataSource.listPayment();
+
+      // Verify the result
+      expect(result, isA<List<PaymentModel>>());
+      expect(result.length, 2);
+      expect(result[0].bankId, 4);
+      expect(result[0].bankName, 'Al Rajhi');
+      expect(result[1].bankId, 4);
+      expect(result[1].bankName, 'Al Arabi');
+
+      // Verify that the service method was called
+      verify(mockWithdrawService.listPayment()).called(1);
+    });
+
+    test('confirmWithDraw handles error', () async {
+      // Simulate a service error
+      when(mockWithdrawService.confirmWithDraw())
+          .thenThrow(Exception('Network Error'));
+
+      expect(() => dataSource.confirmWithDraw(), throwsA(isA<Exception>()));
+    });
+
+    test('listPayment handles error', () async {
+      when(mockWithdrawService.listPayment())
+          .thenThrow(Exception('Network Error'));
+
+      expect(() => dataSource.listPayment(), throwsA(isA<Exception>()));
     });
   });
 }
